ab2944a05e626b9357c63735620f2eb24b303c78
gregaiosa <gregory.aiosa@gmail.com>
2026-02-25 02:25:42 +0000

    MVP for following person

--- launch/nav2.launch.py
+++ launch/nav2.launch.py
@@ -1,115 +1,57 @@
-# Software License Agreement (BSD)
-#
-# @author    Roni Kreinin <rkreinin@clearpathrobotics.com>
-# @copyright (c) 2023, Clearpath Robotics, Inc., All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name of Clearpath Robotics nor the names of its contributors
-#   may be used to endorse or promote products derived from this software
-#   without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from clearpath_config.clearpath_config import ClearpathConfig
 from clearpath_config.common.utils.yaml import read_yaml
-
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    GroupAction,
-    IncludeLaunchDescription,
-    OpaqueFunction
-)
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch.substitutions import (
-    LaunchConfiguration,
-    PathJoinSubstitution
-)
-
-from launch_ros.actions import PushRosNamespace, SetRemap, Node
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import SetRemap
 from nav2_common.launch import RewrittenYaml
 
-
 ARGUMENTS = [
-    DeclareLaunchArgument('use_sim_time', default_value='false',
-                          choices=['true', 'false'],
-                          description='Use sim time'),
-    DeclareLaunchArgument('setup_path',
-                          default_value='/etc/clearpath/',
-                          description='Clearpath setup path'),
-    DeclareLaunchArgument('scan_topic',
-                          default_value='',
-                          description='Override the default 2D laserscan topic')
+    DeclareLaunchArgument('use_sim_time', default_value='false', choices=['true', 'false']),
+    DeclareLaunchArgument('setup_path', default_value='/etc/clearpath/'),
+    DeclareLaunchArgument('scan_topic', default_value=''),
+    DeclareLaunchArgument('robot_base_frame', default_value='chassis_link')
 ]
 
-
 def launch_setup(context, *args, **kwargs):
-    # Packages
     pkg_clearpath_nav2_demos = get_package_share_directory('clearpath_nav2_demos')
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
 
-    # Launch Configurations
     use_sim_time = LaunchConfiguration('use_sim_time')
     setup_path = LaunchConfiguration('setup_path')
     scan_topic = LaunchConfiguration('scan_topic')
+    robot_base_frame = LaunchConfiguration('robot_base_frame')
 
-    # Read robot YAML
     config = read_yaml(os.path.join(setup_path.perform(context), 'robot.yaml'))
-    # Parse robot YAML into config
     clearpath_config = ClearpathConfig(config)
-
     namespace = clearpath_config.system.namespace
     platform_model = clearpath_config.platform.get_platform_model()
 
-    # see if we've overridden the scan_topic
     eval_scan_topic = scan_topic.perform(context)
     if len(eval_scan_topic) == 0:
         eval_scan_topic = f'/{namespace}/sensors/lidar2d_0/scan'
 
-    file_parameters = PathJoinSubstitution([
-        pkg_clearpath_nav2_demos,
-        'config',
-        platform_model,
-        'nav2.yaml'])
+    file_parameters = PathJoinSubstitution([pkg_clearpath_nav2_demos, 'config', platform_model, 'nav2.yaml'])
 
     rewritten_parameters = RewrittenYaml(
         source_file=file_parameters,
         param_rewrites={
-            # the only *.topic parameters are scan.topic, so rewrite all of them to point to
-            # our desired scan_topic
-            'topic': eval_scan_topic,
+            'topic': eval_scan_topic, 
+            'robot_base_frame': robot_base_frame,
+            'base_frame': robot_base_frame 
         },
         convert_types=True
     )
 
-    launch_nav2 = PathJoinSubstitution(
-      [pkg_nav2_bringup, 'launch', 'navigation_launch.py'])
+    launch_nav2 = PathJoinSubstitution([pkg_nav2_bringup, 'launch', 'navigation_launch.py'])
 
-    nav2 = GroupAction([
-        PushRosNamespace(namespace),
-        SetRemap('/' + namespace + '/odom',
-                 '/' + namespace + '/platform/odom'),
+    return [GroupAction([
+        SetRemap('base_link', robot_base_frame),
+        SetRemap('/' + namespace + '/odom', '/' + namespace + '/platform/odom'),
+        SetRemap('map', '/' + namespace + '/map'),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(launch_nav2),
@@ -117,34 +59,10 @@ def launch_setup(context, *args, **kwargs):
                 ('use_sim_time', use_sim_time),
                 ('params_file', rewritten_parameters),
                 ('use_composition', 'False'),
-                ('namespace', namespace)
-              ]
+                ('namespace', namespace),
+            ]
         ),
-        # The following server node
-        Node(
-            package='opennav_following',
-            executable='opennav_following',
-            name='following_server',
-            output='screen',
-            parameters=[rewritten_parameters, {'use_sim_time': use_sim_time}],
-        ),
-
-        # Dedicated lifecycle manager for the following server
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='following_lifecycle_manager',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': True},
-                        {'node_names': ['following_server']}]
-        ),
-    ])
-
-    return [nav2]
-
+    ])]
 
 def generate_launch_description():
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(OpaqueFunction(function=launch_setup))
-    return ld
+    return LaunchDescription(ARGUMENTS + [OpaqueFunction(function=launch_setup)])
